aea803f44459ad1eccc14f630680561d0a1abc4a
nik <nikgajsek31@gmail.com>
2023-05-11 07:58:57 +0200

    hihita

--- db/schema.rb
+++ db/schema.rb
@@ -28,34 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_161644) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -198,7 +170,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_161644) do
     t.datetime "updated_at", null: false
     t.json "image"
     t.integer "user_id"
-    t.string "pdf"
   end
 
   create_table "users", force: :cascade do |t|
@@ -214,18 +185,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_161644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-<<<<<<< HEAD
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-=======
     t.string "city"
     t.string "country"
->>>>>>> 18095ec93fd7043f93a19760517ab5026b4d76ff
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "products"
   add_foreign_key "pay_charges", "pay_customers", column: "customer_id"
