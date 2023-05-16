7ad9dc4a2312681fa8d997d8e3443a3a73b881c9
matej poje <matej7poje@gmail.com>
2023-05-16 09:24:01 +0200

    stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,5 @@
 Rails.configuration.stripe = {
-<<<<<<< HEAD
   :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key],    
   :secret_key => Rails.application.credentials.stripe[:stripe_secret_key]    
 }    
 Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
-=======
-  :publishable_key => ENV['public_key'],
-  :secret_key      => ENV['private_key']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:stripe_secret_key]
-
->>>>>>> aad343ae79fb3ec84a4d3b0e830e531467c84dbd
